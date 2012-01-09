f78598eb4d460b123ccb34928d112144a7d3e884
teslamint <teslamint@gmail.com>
2012-01-09 23:51:27 +0900

    add xml request to query for title and more.

--- SKCommsVideo.js
+++ SKCommsVideo.js
@@ -1,56 +1,101 @@
 addKiller("SKCommsVideo", {
   "canKill": function(data) {
-    if (/v.nate.com/.test(data.src)) { data.site="nate"; return true;}
-    if (/v.egloos.com/.test(data.src)) { data.site="egloos"; return true;}
-    if (/dbi.video.cyworld.com/.test(data.src)) { data.site="cyworld"; return true; }
-    return false;
+    if (/v\.nate\.com/.test(data.src)) {
+      data.site = "nate";
+      return true;
+    } else if (/v\.egloos\.com/.test(data.src)) {
+      data.site = "egloos";
+      return true;
+    } else if (/dbi\.video\.cyworld\.com/.test(data.src)) {
+      data.site = "cyworld";
+      return true;
+    } else return false;
   },
 
   "process": function(data, callback) {
-     var flashvars = parseFlashVariables(data.params.flashvars);
-    // nate (pann, video, ...)
-    if(data.site=="nate") {
-      if(flashvars.mov_id) {
-        this.processNateVideoID(flashvars.mov_id, callback);
+    var flashvars = parseFlashVariables(data.params.flashvars);
+    var mov_id, vs_keys, v_key, url;
+    if (flashvars.v_key) {
+      mov_id = flashvars.mov_id;
+      vs_keys = flashvars.vs_keys;
+      v_key = flashvars.v_key;
+      url = "";
+      if (data.site == "nate") {
+        url = "http://v.nate.com/movie_url.php?vs_id=movie&vs_keys=" + vs_keys + "&mov_id=" + mov_id + "&v_key=" + v_key;
+      } else {
+        if (data.site == "egloos") {
+          url = "http://v.egloos.com/xmovie_url.php?vs_id=movie&vs_keys=" + vs_keys + "&mov_id=" + mov_id + "&v_key=" + v_key;
+        } else {
+          url = "http://v.nate.com/xmovie_url.php?vs_id=movie&vs_keys=" + vs_keys + "&mov_id=" + mov_id + "&v_key=" + v_key;
+        }
       }
-
-      // embedded Nate video
-      var match = data.src.replace(/\|/g, "%7C").match(/v\.nate\.com\/v\.sk\/movie\/0%7C(\d+)\/(\d+)/);
+      this.processNateVideoXml(url, callback);
+    } else {
+      var blogid, serial;
+      var headers = this.getResponseHeader(data.src);
+      var match = data.src.replace(/\|/g, "%7C").match(/(dbi\.video|v)\.(cyworld|nate|egloos))\.com\/v\.sk\/(movie|egloos)\/(0|[a-z]\d+)%7C(\d+)\/(\d+)/);
+      // nate (pann, video, ...)
       if (match) {
-        this.processNateVideoID(match[2], callback);
-      }
-      return;
-    }
-    // Cyworld (merged by Nate)
-    if(data.site=="cyworld") {
-      if(flashvars.mov_id) {
-        this.processNateVideoID(flashvars.mov_id, callback);
-      }
-      return;
-    }
+        blogid = match[3];
+        serial = match[4]
+        vs_keys = match[5];
+        mov_id = match[6];
+        v_key = /v_key=([0-9a-f]+)/.match(headers)[1];
 
-    // egloos (blog)
-    if(data.site=="egloos") {
-      if (flashvars.mov_id && flashvars.vs_keys) {
-        // split vs_keys to blogid & serial
-        var vs_keys = flashvars.vs_keys.split("|");
-        this.processEgloosVideoID(flashvars.mov_id, vs_keys[0], vs_keys[1], callback);
+        if (v_key) {
+          if (data.site == "egloos") {
+            url = "http://v.egloos.com/xmovie_url.php?vs_id=movie&vs_keys=" + vs_keys + "&mov_id=" + mov_id + "&v_key=" + v_key;
+          } else {
+            url = "http://v.nate.com/xmovie_url.php?vs_id=movie&vs_keys=" + vs_keys + "&mov_id=" + mov_id + "&v_key=" + v_key;
+          }
+          this.processNateVideoXml(url, callback);
+        } else if (data.site == "egloos") {
+          // embedded Egloo Video
+          this.processEgloosVideoID(blogid, serial, mov_id, callback);
+        } else {
+          this.processNateVideoID(mov_id, callback);
+        }
       }
-
-      // embedded Egloos Video
-      var match = data.src.replace(/\|/g, "%7C").match(/v\.egloos\.com\/v\.sk\/egloos\/([a-z]\d+)%7C(\d+)\/(\d+)/);
-      if (match) {
-        this.processEgloosVideoID(match[3], match[1], match[2], callback);
-      }
-      return;
     }
+  },
+
+  "processNateVideoXml": function(xml_url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', xml_url, true);
+    xhr.onload = function(event) {
+      var result = event.target.responseXML.getElementsByTagName("movie")[0];
+      var siteinfo = [];
+
+      var title = result.getElementsByTagName("title")[0].textContent;
+      // if title exists, it's embedded player on somewhere but not on Nate.
+      if (title) {
+        var org_url = result.getElementsByTagName("org_url")[0].textContent;
+        var org_name = (/egloos\.com/.test(xml_url)) ? "egloos" : "nate";
+        siteinfo.push({"name": org_name, "url": org_url});
+      }
+      var mov_url = result.getElementsByTagName("mov_urls")[0].getElementsByTagName("url")[0].textContent;
+      var thumb_url = result.getElementsByTagName("master_thumbnail")[0].getElementsByTagName("url")[0].textContent;
+
+      callback({
+        "playlist": [{
+          "title": title,
+          "poster": thumb_url,
+          "siteinfo": siteinfo,
+          "sources": [{
+            "url": mov_url,
+            "isNative": (/mp4/.test(mov_url)) ? true : false
+          }]
+        }]
+      });
+    };
+    xhr.send(null);
   },
 
   "processNateVideoID": function(videoid, callback) {
     callback({
       "playlist": [{
         "sources": [{
-          "url": "http://m.pann.nate.com/video/videoPlayUrlRealTime?video_id="+videoid+"",
+          "url": "http://m.pann.nate.com/video/videoPlayUrlRealTime?video_id=" + videoid + "",
           "isNative": true
         }]
       }]
@@ -61,10 +106,18 @@ addKiller("SKCommsVideo", {
     callback({
       "playlist": [{
         "sources": [{
-          "url": "http://ebc.egloos.com/exec/mobile/play_movile_video.php?movieid="+videoid+"&blogid="+blogid+"&serial="+serial+"",
+          "url": "http://ebc.egloos.com/exec/mobile/play_movile_video.php?movieid=" + videoid + "&blogid=" + blogid + "&serial=" + serial + "",
           "isNative": true
         }]
       }]
     });
+  },
+  
+  "getResponseHeader": function(src) {
+    // trying to get response header
+    var xhr = new XMLHttpRequest();
+    xhr.get('GET', src, false);
+    xhr.send(null);
+    return xhr.getAllResponseHeaders().toLowerCase();
   }
 });

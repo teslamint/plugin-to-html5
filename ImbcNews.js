1fc50b0379e0babe6f4bc588d0121fe089b019f5
teslamint <teslamint@gmail.com>
2012-09-02 22:20:23 +0900

    add poster url;

--- ImbcNews.js
+++ ImbcNews.js
@@ -12,10 +12,12 @@ addKiller("ImbcNews", {
 	if(match) {
 		var start_time = flashvars.startTime;
 		var end_time = flashvars.endTime;
+		var img_url = flashvars.imgUrl;
 		// http://vod.imnews.imbc.com/vod/_definst_/mp4:newsvod/desk/2012/09/mbc_desk_20120902_3_500k.mp4/playlist.m3u8
 		var url = "http://"+ match[1] +".mp4/playlist.m3u8?wowzaplaystart="+ start_time +"&wowzaplayduration="+ (end_time - start_time);
 		callback({
 			"playlist": [{
+				"poster": imgUrl,
 				"sources": [{
 					"url": url,
 					"format": "MP4",

09a742a23167f37cc323be1706786a947848a818
teslamint <teslamint@gmail.com>
2012-09-02 22:17:10 +0900

    add player variable;

--- ImbcNews.js
+++ ImbcNews.js
@@ -10,8 +10,10 @@ addKiller("ImbcNews", {
 	// rtmp://vod.imnews.imbc.com/vod/_definst_/mp4:newsvod/desk/2012/09/mbc_desk_20120902_3_500k.mp4
 	var match = flashvars.vodUrl.match(/^rtmp:\/\/(.+)\.mp4$/i);
 	if(match) {
+		var start_time = flashvars.startTime;
+		var end_time = flashvars.endTime;
 		// http://vod.imnews.imbc.com/vod/_definst_/mp4:newsvod/desk/2012/09/mbc_desk_20120902_3_500k.mp4/playlist.m3u8
-		var url = "http://"+ match[1] +".mp4/playlist.m3u8";
+		var url = "http://"+ match[1] +".mp4/playlist.m3u8?wowzaplaystart="+ start_time +"&wowzaplayduration="+ (end_time - start_time);
 		callback({
 			"playlist": [{
 				"sources": [{
